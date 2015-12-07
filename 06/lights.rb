9071009d6aa125ae0a83723dabd4eced49198b36
Adam Bachman <adam.bachman@gmail.com>
2015-12-07 10:06:40 -0500

    add day 06

---
+++ 06/lights.rb
@@ -0,0 +1,163 @@
+# --- Day 6: Probably a Fire Hazard ---
+#
+# Because your neighbors keep defeating you in the holiday house decorating
+# contest year after year, you've decided to deploy one million lights in a
+# 1000x1000 grid.
+#
+# Furthermore, because you've been especially nice this year, Santa has mailed
+# you instructions on how to display the ideal lighting configuration.
+#
+# Lights in your grid are numbered from 0 to 999 in each direction; the lights
+# at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions
+# include whether to turn on, turn off, or toggle various inclusive ranges
+# given as coordinate pairs. Each coordinate pair represents opposite corners
+# of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore
+# refers to 9 lights in a 3x3 square. The lights all start turned off.
+#
+# To defeat your neighbors this year, all you have to do is set up your lights
+# by doing the instructions Santa sent you in order.
+#
+# For example:
+#
+# - turn on 0,0 through 999,999 would turn on (or leave on) every light.
+# - toggle 0,0 through 999,0 would toggle the first line of 1000 lights,
+#   turning off the ones that were on, and turning on the ones that were off.
+# - turn off 499,499 through 500,500 would turn off (or leave off) the middle
+#   four lights.
+#
+# After following the instructions, how many lights are lit?
+#
+# --- Part Two ---
+#
+# You just finish implementing your winning light pattern when you realize you
+# mistranslated Santa's message from Ancient Nordic Elvish.
+#
+# The light grid you bought actually has individual brightness controls; each
+# light can have a brightness of zero or more. The lights all start at zero.
+#
+# The phrase turn on actually means that you should increase the brightness of
+# those lights by 1.
+#
+# The phrase turn off actually means that you should decrease the brightness of
+# those lights by 1, to a minimum of zero.
+#
+# The phrase toggle actually means that you should increase the brightness of
+# those lights by 2.
+#
+# What is the total brightness of all lights combined after following Santa's
+# instructions?
+#
+# For example:
+#
+# - turn on 0,0 through 0,0 would increase the total brightness by 1.
+# - toggle 0,0 through 999,999 would increase the total brightness by 2000000.
+#
+
+in_file = open('input.txt').readlines
+
+# Examples:
+#   turn on 668,20 through 935,470
+#   turn off 487,286 through 540,328
+#   toggle 247,874 through 840,955
+matcher = /(turn on|turn off|toggle) (\d+,\d+) through (\d+,\d+)/
+
+def ranges_to_coords(range_start, range_end)
+  sx, sy = range_start.split(',').map(&:to_i)
+  ex, ey = range_end.split(',').map(&:to_i)
+
+  return {
+    sx: sx,
+    sy: sy,
+    ex: ex,
+    ey: ey,
+  }
+end
+
+def turn_on(grid, coords)
+  (coords[:sy]..coords[:ey]).each do |y|
+    (coords[:sx]..coords[:ex]).each do |x|
+      # v1
+      # grid[y][x] = 1
+
+      # v2
+      grid[y][x] += 1
+    end
+  end
+end
+
+def turn_off(grid, coords)
+  (coords[:sy]..coords[:ey]).each do |y|
+    (coords[:sx]..coords[:ex]).each do |x|
+      # v1
+      # grid[y][x] = 0
+
+      # v2
+      if grid[y][x] > 0
+        grid[y][x] -= 1
+      end
+    end
+  end
+end
+
+def toggle(grid, coords)
+  (coords[:sy]..coords[:ey]).each do |y|
+    (coords[:sx]..coords[:ex]).each do |x|
+      # v1
+      # grid[y][x] = grid[y][x] == 1 ? 0 : 1
+
+      # v2
+      grid[y][x] += 2
+    end
+  end
+end
+
+def count_lights(grid)
+  total = 0
+
+  grid.each do |row|
+    row.each do |col|
+      total += col
+    end
+  end
+
+  return total
+end
+
+# initialize grid [
+#                   [0, 0, 0, ...],
+#                   [0, 0, 0, ...],
+#                   [0, 0, 0, ...],
+#                   [0, 0, 0, ...],
+#                   ...
+#                 ]
+grid = (0..999).map {|y|
+  (0..999).map {|x|
+    0
+  }
+}
+
+in_file.each do |line|
+  _, instruction, range_start, range_end = matcher.match(line).to_a
+
+  coords = ranges_to_coords(range_start, range_end)
+
+  # puts "DO #{ instruction } FROM #{ range_start } TO #{ range_end }"
+
+  case instruction
+  when 'turn on'
+    turn_on(grid, coords)
+  when 'turn off'
+    turn_off(grid, coords)
+  when 'toggle'
+    toggle(grid, coords)
+  else
+    puts "SYNTAX ERROR"
+  end
+end
+
+# v1
+# puts "#{ count_lights(grid) } LIGHTS ARE LIT"
+
+# v2
+puts "BRIGHTNESS LEVEL: #{ count_lights(grid) }"
+
