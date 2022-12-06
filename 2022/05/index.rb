29584de7bec58d751cadecc10576b7604996a549
Adam Bachman <abachman@recurly.com>
2022-12-06 11:38:43 -0500

    clean up whitespace

--- 2022/05/index.rb
+++ 2022/05/index.rb
@@ -2,10 +2,10 @@ PART_ONE = false
 
 # DEMO
 #
-#     [D]    
-# [N] [C]    
+#     [D]
+# [N] [C]
 # [Z] [M] [P]
-#  1   2   3 
+#  1   2   3
 
 infile = <<EOS
 move 1 from 2 to 1
@@ -15,7 +15,7 @@ move 1 from 1 to 2
 EOS
 
 stacks = [
-  nil, 
+  nil,
   %w(Z N),
   %w(M C D),
   %w(P)
@@ -23,19 +23,19 @@ stacks = [
 
 # ACTUAL
 #
-#     [W]         [J]     [J]        
-#     [V]     [F] [F] [S] [S]        
-#     [S] [M] [R] [W] [M] [C]        
+#     [W]         [J]     [J]
+#     [V]     [F] [F] [S] [S]
+#     [S] [M] [R] [W] [M] [C]
 #     [M] [G] [W] [S] [F] [G]     [C]
 # [W] [P] [S] [M] [H] [N] [F]     [L]
 # [R] [H] [T] [D] [L] [D] [D] [B] [W]
 # [T] [C] [L] [H] [Q] [J] [B] [T] [N]
 # [G] [G] [C] [J] [P] [P] [Z] [R] [H]
-#  1   2   3   4   5   6   7   8   9 
-#
+#  1   2   3   4   5   6   7   8   9
+
 infile = File.open('input.txt').read
 stacks = [
-  nil, 
+  nil,
   %w(G T R W),
   %w(G C H P M S V W),
   %w(C L T S G M),
@@ -47,7 +47,7 @@ stacks = [
   %w(H N W L C)
 ]
 
-def disp(stx) 
+def disp(stx)
   stx[1..-1].each_with_index do |l, i|
     puts format("%i #{l.join(' ')}", i+1)
   end
@@ -62,10 +62,10 @@ infile.lines.each_with_index do |line, i|
   # puts format("%04i M %2i F %2i T %2i", i, move, from, to)
 
   mid = []
-  move.times do 
+  move.times do
     c = stacks[from].pop
 
-    if PART_ONE 
+    if PART_ONE
       mid << c
     else
       mid.unshift c
