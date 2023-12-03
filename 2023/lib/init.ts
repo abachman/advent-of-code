bb70318156acf128e4b062d4d1d808a06b1422d7
Adam Bachman <adam.bachman@gmail.com>
2023-12-03 15:15:22 -0500

    templating for new days

--- 2023/lib/init.ts
+++ 2023/lib/init.ts
@@ -3,11 +3,38 @@ import { join } from 'std/path/join.ts'
 
 const day = Deno.args[0]
 
+const t = (s: string, d: Record<string, string | number>) => {
+  Object.keys(d).forEach((k) => {
+    const v = d[k]
+    s = s.replace(new RegExp(`{{${k}}}`, 'g'), String(v))
+  })
+  return s
+}
+
+const template = `
+// https://adventofcode.com/2023/day/{{nday}}
+// {{day}}/index.ts
+
+import { setup, log } from '../lib/setup.ts'
+
+const { PART, INPUT } = await setup('{{day}}')
+
+if (PART === 'one') {
+  log('one', INPUT)
+} else {
+  log('two', INPUT)
+}
+`
+
 if (/^[0-9][0-9]$/.test(day)) {
   const dir = `./${day}`
   await ensureDir(dir)
 
-  ;['input.txt', 'example.txt', 'index.ts'].forEach(async (file: string) => {
+  ;['input.txt', 'example.txt', 'index.ts', 'description.md'].forEach(async (file: string) => {
     await ensureFile(join(day, file))
   });
+
+  const nday = Number(day)
+  const index = t(template, { day, nday })
+  await Deno.writeTextFile(join(day, 'index.ts'), index)
 }
